e06d66c3d780ca639570ae3b6b2665fa532e9fc3
Cosmin Radoi <cosmin.radoi@gmail.com>
2012-11-15 18:08:19 -0600

    build using Common

---
+++ project/Build.scala
@@ -0,0 +1,19 @@
+import sbt._
+import Keys._
+
+object WorkspaceBuild extends Build with Common {
+  lazy val workspace = Project(id = "workspace",
+    base = file(".")) aggregate (iteRace)
+
+  lazy val iteRace = Project(id = "IteRace",
+    base = file("IteRace")) dependsOn (
+      util,
+      walaFacade,
+      walaUtil,
+      walaShrike,
+      walaCore, 
+      parallelArrayMock)
+
+  lazy val parallelArrayMock = Project(id = "ParallelArray-mock",
+    base = file("ParallelArray-mock"))
+}
