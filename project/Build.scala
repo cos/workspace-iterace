6e3dc98816c012fab2a285012c83312bdb648b1b
Cosmin Radoi <cosmin.radoi@gmail.com>
2012-12-22 20:21:29 -0600

    running the evaluation kind of works

--- project/Build.scala
+++ project/Build.scala
@@ -1,9 +1,12 @@
 import sbt._
 import Keys._
+import Build.data
 
 object WorkspaceBuild extends Build with Common with Evaluate {
   lazy val workspace = Project(id = "workspace",
-    base = file(".")) aggregate (iteRace)
+    base = file("."),
+    settings = Project.defaultSettings ++ Seq(benchTask))
+    .aggregate(iteRace)
 
   lazy val iteRace = Project(id = "IteRace",
     base = file("IteRace")) dependsOn (
@@ -11,9 +14,33 @@ object WorkspaceBuild extends Build with Common with Evaluate {
       walaFacade,
       walaUtil,
       walaShrike,
-      walaCore, 
+      walaCore,
       parallelArrayMock)
 
   lazy val parallelArrayMock = Project(id = "ParallelArray-mock", base = file("lib/parallelArray.mock"))
+
+  val projectAxis = StringAxis("project", "project", List("BH", "EM3D"))
   
+  def axes = List(projectAxis) ++ (
+    List("two-threads", "known-safe-filtering", "bubble-up", "deep-synchronized", "app-level-synchronized") map { axis =>
+    BooleanAxis(axis, "iterace." + axis)
+  })
+
+  object Keys {
+    val bench = InputKey[Unit]("bench")
+  }
+
+  lazy val benchTask = Keys.bench <<= InputTask(parser)(benchDef)
+
+  lazy val benchDef = (parsed: TaskKey[Scenario]) => {
+    (fullClasspath in (iteRace, Test), mainClass in (iteRace, Test, run), runner in (iteRace, Runtime, run), streams, parsed) map { (cp, mc, r, s, scenario) => { 
+      	val arguments = Seq(scenario(projectAxis).asInstanceOf[String])
+      	scenario foreach {println(_)}
+      	val options = scenario filter { case (_, v:Boolean) => v; case _ => false } map {case (k, _) => k.toString}
+      	s.log.info(options.toString)
+      	toError(r.run(mc.get, data(cp), arguments ++ options, s.log))
+      }
+    }
+  }
+
 }
